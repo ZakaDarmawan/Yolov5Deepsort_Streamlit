54fee688b1f43a85b455dee5b387b62109d12bbc
ZakaDarmawan <108005838+ZakaDarmawan@users.noreply.github.com>
2024-05-03 14:48:36 +0700

    Update track.py

--- track.py
+++ track.py
@@ -312,11 +312,11 @@ def reset():
     
 # Mendapatkan jalur absolut ke direktori skrip
 script_dir = os.path.dirname(os.path.abspath(__file__))
-default_model_path = os.path.join(script_dir, 'yolov5/runs/train/exp/weights/last.pt')
+default_model_path = os.path.join(script_dir, 'last.pt')
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--yolo_model', nargs='+', type=str, default='yolov5/runs/train/exp/weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--yolo_model', nargs='+', type=str, default='last.pt', help='model.pt path(s)')
     parser.add_argument('--deep_sort_model', type=str, default='osnet_x0_25')
     parser.add_argument('--source', type=str, default='videos/motor.mp4', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
