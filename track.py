eda106eac8216ac49c7dda5a2d5ded3d9ea1df42
ZakaDarmawan <108005838+ZakaDarmawan@users.noreply.github.com>
2024-05-30 13:34:14 +0700

    Update track.py

--- track.py
+++ track.py
@@ -34,7 +34,6 @@ from yolov5.utils.torch_utils import select_device, time_sync
 from yolov5.utils.plots import Annotator, colors
 from deep_sort.utils.parser import get_config
 from deep_sort.deep_sort import DeepSort
-from .deep_sort import DeepSort
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # yolov5 deepsort root directory
